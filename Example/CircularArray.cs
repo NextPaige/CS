8b63af6ccef33e43374b77dfad354131adbecf14
inandexhale95 <inandexhale95@gmail.com>
2021-01-07 03:50:03 +0900

    원형 배열

---
+++ Example/CircularArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    // FIFO(First in First out)
+    // index = (index + i) % Array.Length;
+    class CircularArray
+    {
+        char[] A = "abcdefgh".ToCharArray();
+        int startIndex = 2;
+        
+        public void CheckArray()
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                int index = (startIndex + i) % A.Length;
+                Console.WriteLine(A[index]);
+            }
+        }
+    }
+}
