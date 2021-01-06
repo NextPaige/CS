37bcc788501730f11a79866e273d57274b3df953
inandexhale95 <inandexhale95@gmail.com>
2021-01-07 03:47:55 +0900

    C# 동적 배열

---
+++ Example/JaggedArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class JaggedArray
+    {
+        private object[] arr = new object[0];
+
+        public void Add(object element)
+        {
+            var temp = new object[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp[i] = arr[i];
+            }
+            arr = temp;
+
+            arr[arr.Length - 1] = element;
+        }
+    }
+}
