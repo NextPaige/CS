a04850f272f55b91464a53dd5273f7e555b0c10c
inandexhale95 <inandexhale95@gmail.com>
2021-01-08 01:43:17 +0900

    원형 배열 연결리스트

---
+++ Example/QueueUsingLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class Node
+    {
+        public object Data { get; set; }
+        public Node Next { get; set; }
+        public Node(object data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    class QueueUsingLinkedList
+    {
+        private Node head = null;
+        private Node tail = null;
+
+        public void Enqueue(object data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+                tail = head;
+            }
+            else
+            {
+                tail.Next = new Node(data);
+                tail = tail.Next;
+            }
+        }
+
+        public object Dequeue()
+        {
+            if (head == null)
+            {
+                throw new ApplicationException("Empty");
+            }
+
+            object data = head.Data;
+
+            // Queue에 하나 남은 경우
+            if (head == tail)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                head = head.Next;
+            }
+            return data;
+        }
+
+    }
+}
